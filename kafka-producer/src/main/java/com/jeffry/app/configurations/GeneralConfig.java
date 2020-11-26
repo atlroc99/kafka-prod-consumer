eb1291f044abbfdf2a576d1466539ad6e62c48c8
mohammad zaman <azaman@atgworks.com>
2020-11-26 12:10:09 -0500

    closes #2

---
+++ kafka-producer/src/main/java/com/jeffry/app/configurations/GeneralConfig.java
@@ -0,0 +1,14 @@
+package com.jeffry.app.configurations;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "app", ignoreInvalidFields = false)
+public class GeneralConfig {
+
+    private String topic;
+}
