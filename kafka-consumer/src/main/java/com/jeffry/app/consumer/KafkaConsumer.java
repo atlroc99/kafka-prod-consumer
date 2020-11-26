42065369381d0f934c667675a7054cbfdb11a1b4
mohammad zaman <azaman@atgworks.com>
2020-11-26 13:28:26 -0500

    closes #3

--- kafka-consumer/src/main/java/com/jeffry/app/consumer/KafkaConsumer.java
+++ kafka-consumer/src/main/java/com/jeffry/app/consumer/KafkaConsumer.java
@@ -1,6 +1,5 @@
 package com.jeffry.app.consumer;
 
-import com.jeffry.app.configurations.GeneralConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
